8700b3c678202f6cff2df483fe485668f4788224
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-14 02:35:44 +0530

    Program to print 5 times marvellous on screen.

---
+++ Cpp_Programming/Competative/Assignments/Assignment_1/program2.cpp
@@ -0,0 +1,21 @@
+//program to print 5 times marvellous on screen
+
+#include<iostream>
+using namespace std;
+
+void Display()
+{
+    int iCnt = 0;
+
+    for(iCnt = 0; iCnt <= 5; iCnt++)
+    {
+        cout<<"Marvellous\n";
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
