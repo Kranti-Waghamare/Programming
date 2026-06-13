a074f539a897553c26ee2e1c4512a821bc78a101
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-14 02:48:27 +0530

    program to print 5 times marvellous on screen.

---
+++ Java_Programming/Competative/Assignments/Assignment_1/program2.java
@@ -0,0 +1,16 @@
+class program2
+{
+    public static void Display()
+    {
+        int iCnt = 0;
+
+        for(iCnt = 0; iCnt <= 5; iCnt++)
+        {
+            System.out.println("Marvellous");
+        }
+    }
+    public static void main(String A[])
+    {
+        Display();
+    }
+}
