99732831a533d15251edef43667a92112c6d50cf
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-19 03:00:12 +0530

    program to display 5 to 1 numbers on the screen

---
+++ Java_Programming/Competative/Assignments/Assignment_1/program3.java
@@ -0,0 +1,23 @@
+//program to display 5 to 1 numbers on the screen
+
+class program3 
+{
+    static void Display()
+    {
+        int i = 0;
+
+        i = 5;
+
+        while( i >= 1)
+        {
+        System.out.println(i);
+            i--;
+        }
+    }
+
+
+    public static void main(String A[])
+    {
+        Display();
+    }
+}
