43a31d2e7310f37fe4e75fd5b580767734ba6a66
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-19 23:42:27 +0530

    Addition two numbers

---
+++ Cpp_Programming/Practice/LB_Class/program1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i, j, k;
+
+    cout<<"Enter the first number : \n";
+    scanf("%d", &i);
+
+    cout<<"Enter the second number : \n";
+    cin>>j;
+
+    k = i + j;
+
+    cout<<"Addition is : %d\n", k;
+
+    return 0;
+}
