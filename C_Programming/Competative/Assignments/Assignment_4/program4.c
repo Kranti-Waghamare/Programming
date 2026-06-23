3c581e0d661ab801906c671f265b0d886b76f9e9
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-23 22:26:11 +0530

    summation of all its non factors

---
+++ C_Programming/Competative/Assignments/Assignment_4/program4.c
@@ -0,0 +1,36 @@
+//Write a program which accept the number from user and returns summation of all its non factors.
+
+//Input : 12
+//Output : (5 + 7 + 8 + 9 + 10 +11 = 50)
+
+#include<stdio.h>
+
+int SumNonFact(int iNo)
+{
+    int iCnt = 0;
+    int iSum = 0;
+
+    for(iCnt = 1; iCnt < iNo; iCnt++)
+    {
+        if((iNo % iCnt) != 0)
+        {
+            iSum = iSum + iCnt;
+        }
+    }
+    return iSum;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d", &iValue);
+
+    iRet = SumNonFact(iValue);
+
+    printf("%d\n", iRet);
+
+    return 0;
+}
