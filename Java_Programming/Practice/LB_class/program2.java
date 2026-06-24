83cd039ee52daf5cbd1f8448f809b9349e123e24
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-24 17:08:35 +0530

    Addition of two numbers

---
+++ Java_Programming/Practice/LB_class/program2.java
@@ -0,0 +1,24 @@
+package Practice.LB_class;
+
+import java.util.*;
+
+class program2 
+{
+    public static void main(String A[])
+    {
+        int i, j, k;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the first number : ");
+        i = sobj.nextInt();
+
+        System.out.println("Enter the second number : ");
+        j = sobj.nextInt();
+
+        k = i + j;
+
+        System.out.println("Addition is : "+k);
+
+    }
+}
