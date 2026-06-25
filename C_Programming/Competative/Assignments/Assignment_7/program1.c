1d7c7291a1dc50e73da2f5689015b4b37b43aa05
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-25 08:18:28 +0530

    accepts the number from user and display pattern.

---
+++ C_Programming/Competative/Assignments/Assignment_7/program1.c
@@ -0,0 +1,35 @@
+//write a program which accepts the number from user and display below pattern.
+//Input : 5
+//Output : * * * * * # # # # #
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("*\t");
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("#\t");
+    }
+    
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d", &iValue);
+
+    Display(iValue);
+
+    return 0;
+}
+
+//Time Complexity : O(N^2)
