ae180287caf24c338a11847ef780c216c105000b
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-25 08:21:21 +0530

    program to find a even factorial of given number

---
+++ C_Programming/Competative/Assignments/Assignment_7/program3.c
@@ -0,0 +1,48 @@
+//Write a program to find a even factorial of given number.
+//Input : 5
+//Output : 8 (4 * 2)
+
+#include<stdio.h>
+
+int EvenFactorial(int iNo)
+{
+    int iCnt = 0;
+    int iMult = 1;
+    
+    if(iNo == 0)
+    {
+        return 1;
+    }
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        if((iCnt % 2 ) == 0)
+        {
+            iMult = iMult * iCnt;
+        }
+    }
+    return iMult;
+
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d", &iValue);
+
+    iRet = EvenFactorial(iValue);
+
+    printf("Even factorial of number is : %d\n", iRet);
+
+    return 0;
+}
+
+//Time Complexity : O(N)
