77d831279f2441e71e383c5ae87db6ff578c34e3
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-25 07:51:04 +0530

    accepts number from user and print that number of $ and * on screen

---
+++ C_Programming/Competative/Assignments/Assignment_5/program1.c
@@ -0,0 +1,27 @@
+//write a program a which accepts number from user and print that number of $ and * on screen
+//Input : 5
+//Output : $ * $ * $ * $ * $ *
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("$\t*\t");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d", &iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
