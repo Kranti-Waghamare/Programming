27b399b9650e38e4406a29863792e375cd4e9c8a
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-06-26 23:44:45 +0530

    accept number from user and return the count of even digits.

---
+++ C_Programming/Competative/Assignments/Assignment_9/program1.c
@@ -0,0 +1,40 @@
+//write a program which accept number from user and return the count of even digits. 
+//Input : 2395
+//Output : 1
+
+#include<stdio.h>
+
+int CountEven(int iNo)
+{
+    int iDigit = 0;
+    int iCount = 0;
+
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+
+        if((iDigit % 2) == 0)
+        {
+            iCount++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCount;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d", &iValue);
+
+    iRet = CountEven(iValue);
+
+    printf("Even numbers are : %d\n", iRet);
+
+    return 0;
+}
+
+//Time Complexity : O(N)
