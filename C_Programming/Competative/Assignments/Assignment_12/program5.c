e550f2bf70f55bfcd9ef2069eb453099d9e16cd0
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-10 00:41:17 +0530

    accept the number from user and count frequency of such a digits which are less than 6.

---
+++ C_Programming/Competative/Assignments/Assignment_12/program5.c
@@ -0,0 +1,45 @@
+//write a program which accept the number from user and count frequency of such a digits which are less than 6.
+//Input : 2395
+//Output : 3
+
+#include<stdio.h>
+
+int Count(int iNo)
+{
+    int iDigit = 0;
+    int iCount = 0;
+    
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while(iNo > 0)
+    {
+        iDigit = iNo % 10;
+
+        if(iDigit < 6)
+        {
+            iCount++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCount;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the number : \n");
+    scanf("%d", &iValue);
+
+    iRet = Count(iValue);
+
+    printf("%d\n", iRet);
+
+    return 0;
+}
+
+//Time Complexity : O(N)
