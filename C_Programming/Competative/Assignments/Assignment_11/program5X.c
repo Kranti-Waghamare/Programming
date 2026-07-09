621f674aee6161fa3027e4eeab30d7eb7529e70a
Kranti-Waghamare <krantiwaghamare@gmail.com>
2026-07-10 00:35:16 +0530

    display all the numbers in between that range in the reverse manner.using while and for loop

---
+++ C_Programming/Competative/Assignments/Assignment_11/program5X.c
@@ -0,0 +1,32 @@
+//write a program which accepts the range from user and display all the numbers in between that range in the reverse manner.
+//Input : 23 35 
+//Output : 35 34 33 32 31 30 29 28 27 26 25 24 23
+
+#include<stdio.h>
+
+void RangeDisplayRev(int iStart, int iEnd)
+{
+    while(iEnd >= iStart)
+    {
+        printf("%d\n", iEnd);
+        iEnd--;
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter the starting number : \n");
+    scanf("%d", &iValue1);
+
+    printf("Enter the ending number : \n");
+    scanf("%d", &iValue2);
+
+    RangeDisplayRev(iValue1, iValue2);
+
+    return 0;
+}
+
+//Time Complexity : O(N)
